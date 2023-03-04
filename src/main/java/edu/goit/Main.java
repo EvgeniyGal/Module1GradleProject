63620c383ea1de6d4568b9e3519b71d1d318a726
Evgeniy <evgeniygal@gmail.com>
2023-03-04 13:29:35 +0200

    Initial commit

---
+++ src/main/java/edu/goit/Main.java
@@ -0,0 +1,17 @@
+package edu.goit;
+
+import com.google.gson.Gson;
+import edu.goit.user.User;
+
+public class Main {
+    public static void main(String[] args) {
+
+        User user = new User();
+        user.setName("Yevgen");
+        user.setLastName("Galamaga");
+
+        Gson gson = new Gson();
+
+        System.out.println(gson.toJson(user));
+    }
+}
